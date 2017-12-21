b7a591665fd1c873905bfd5ee4afa51d5fac644a
Michael Morfessis <Michael.Morfessis@globalinformationsystems.com>
2017-12-21 09:08:16 -0600

    Added functionality to get post and delete

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 var express = require("express");
+var exphbs = require("express-handlebars");
 // var met = require("method-override");
-// var body = require("body-parser");
+var bodyParser = require("body-parser");
 var mysql = require("mysql");
 
 var PORT = 3000;
 
 var app = express();
 
-// app.use();
-// app.use();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ type: 'application/json' }));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 
 var connection = mysql.createConnection({
 	host: "localhost",
@@ -24,7 +29,46 @@ connection.connect(function(err){
 
 });
 
-connection.query("SELECT * FROM burgers;")
+app.get("/", function(req, res) {
+
+	connection.query("SELECT * FROM burgers;", function(err, data){
+		if (err) { 
+			return res.status(500).end();
+		}
+			res.render("burger", { data: data });
+	})
+
+})
+
+app.post("/burger", function(req, res) {
+	console.log(req.body.burgers);
+
+	connection.query("INSERT INTO burgers (burger_name) VALUES (?);", [req.body.burgers], function(err, data){
+		if (err) { 
+			return res.status(500).end();
+			
+		}
+			res.json({ id: data.insertId });
+    		console.log({ id: data.insertId });
+    		// console.log("This is working")
+	})
+
+})
+
+
+app.delete("/burger/:id", function(req, res) {
+  connection.query("DELETE FROM burgers WHERE id = ?", [req.params.id], function(err, result) {
+    if (err) {
+    
+      return res.status(500).end();
+    } else if (result.affectedRows == 0) {
+  
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
 
 
 app.listen(PORT, function(err){
